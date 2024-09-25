3d174cde26b3c20c2cb44677151974560f79fec8
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-09-25 08:47:36 -0300

    Update test/matrices.jl

--- test/matrices.jl
+++ test/matrices.jl
@@ -4,17 +4,17 @@
   gtb = georef(tab, dom)
 
   # transition count matrix
-  C = GeoStatsFunctions.countmatrix(gtb, :z)
+  C = GeoStatsFunctions.countmatrix(gtb, "z")
   @test size(C) == (3, 3)
   @test eltype(C) == Int
 
   # transition probability matrix
-  T = GeoStatsFunctions.probmatrix(gtb, :z)
+  T = GeoStatsFunctions.probmatrix(gtb, "z")
   @test size(T) == (3, 3)
   @test eltype(T) == Float64
 
   # transition rate matrix
-  R = GeoStatsFunctions.ratematrix(gtb, :z)
+  R = GeoStatsFunctions.ratematrix(gtb, "z")
   @test size(R) == (3, 3)
   @test eltype(R) == typeof(1 / 1u"m")
 end
