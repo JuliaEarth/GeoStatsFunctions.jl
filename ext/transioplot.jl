4e8eef955c3f50c2ecfbc1261a6510b220a59717
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-10-08 08:36:29 -0300

    Improve transioplot

--- ext/transioplot.jl
+++ ext/transioplot.jl
@@ -24,18 +24,30 @@ function transioplot(
   end
 
   # transiogram up to maximum lag
-  x = range(zero(H), stop=H, length=100)
-  y = t.(x)
+  hs = range(zero(H), stop=H, length=100)
+  ts = t.(hs)
+
+  # approximate transiogram at h → ∞
+  t∞ = t(100H)
 
   fig = Makie.Figure()
   for i in 1:L, j in 1:L
     lᵢ, lⱼ = l[i], l[j]
     ax = Makie.Axis(fig[i, j])
-    ys = getindex.(y, i, j)
-    Makie.lines!(ax, x, ys, color=color, linewidth=size, label="$lᵢ → $lⱼ")
+    ys = getindex.(ts, i, j)
+    Makie.lines!(ax, hs, ys, color=color, linewidth=size, label="$lᵢ → $lⱼ")
     if i == j
-      # show effective range
+      # show mean length
       Makie.lines!(ax, [zero(H), r[i]], [1.0, 0.0], color=color, linewidth=size, linestyle=:dash)
+      # show proportion
+      Makie.hlines!(ax, t∞[i, j], color=color, linewidth=size, linestyle=:dash)
+    else
+      # show random transition model
+      pᵢ = t∞[i, i]
+      pⱼ = t∞[j, j]
+      tᵢᵢ = getindex.(ts, i, i)
+      rs = @. (1 - tᵢᵢ) * pⱼ / (1 - pᵢ)
+      Makie.lines!(ax, hs, rs, color=color, linewidth=size, linestyle=:dot)
     end
     Makie.axislegend(position=i == j ? :rt : :rb)
   end
