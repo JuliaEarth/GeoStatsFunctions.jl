beb604024da4bc537145823e31dbc7f0302077c3
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-10-17 16:30:30 -0300

    Add DirectionalTransiogram and PlanarTransiogram

--- src/empirical/transiogram.jl
+++ src/empirical/transiogram.jl
@@ -74,3 +74,86 @@ function EmpiricalTransiogram(
 
   EmpiricalTransiogram(counts, abscissas, ordinates, distance, estim)
 end
+
+"""
+    EmpiricalTransiogram(partition, var₁, var₂=var₁; [parameters])
+
+Compute the empirical (cross-)variogram of the geospatial `partition` for
+variables `var₁` and `var₂` as described in Hoffimann & Zadrozny 2019.
+
+Optionally, forward `parameters` for the underlying [`EmpiricalTransiogram`](@ref).
+"""
+function EmpiricalTransiogram(partition::Partition, var₁, var₂=var₁; kwargs...)
+  # retain geospatial data with at least two elements
+  filtered = Iterators.filter(d -> nelements(domain(d)) > 1, partition)
+  @assert !isempty(filtered) "invalid partition of geospatial data"
+  γ(d) = EmpiricalTransiogram(d, var₁, var₂; kwargs...)
+  tmapreduce(γ, merge, collect(filtered))
+end
+
+"""
+    DirectionalTransiogram(direction, data, var₁, var₂=var₁; dtol=1e-6u"m", [parameters])
+
+Computes the empirical (cross-)variogram for the variables `var₁` and `var₂` stored in
+geospatial `data` along a given `direction` with band tolerance `dtol` in length units.
+
+Optionally, forward `parameters` for the underlying [`EmpiricalTransiogram`](@ref).
+"""
+function DirectionalTransiogram(dir, data::AbstractGeoTable, var₁, var₂=var₁; dtol=1e-6u"m", kwargs...)
+  rng = MersenneTwister(123)
+  Π = partition(rng, data, DirectionPartition(dir; tol=dtol))
+  EmpiricalTransiogram(Π, var₁, var₂; kwargs...)
+end
+
+"""
+    PlanarTransiogram(normal, data, var₁, var₂=var₁; ntol=1e-6u"m", [parameters])
+
+Computes the empirical (cross-)variogram for the variables `var₁` and `var₂` stored in
+geospatial `data` along a plane perpendicular to a `normal` direction with plane
+tolerance `ntol` in length units.
+
+Optionally, forward `parameters` for the underlying [`EmpiricalTransiogram`](@ref).
+"""
+function PlanarTransiogram(normal, data::AbstractGeoTable, var₁, var₂=var₁; ntol=1e-6u"m", kwargs...)
+  rng = MersenneTwister(123)
+  Π = partition(rng, data, PlanePartition(normal; tol=ntol))
+  EmpiricalTransiogram(Π, var₁, var₂; kwargs...)
+end
+
+"""
+    merge(tα, tβ)
+
+Merge the empirical transiogram `tα` with the empirical transiogram `tβ`
+assuming that both transiograms have the same number of lags, distance
+and estimator.
+"""
+function merge(tα::EmpiricalTransiogram{ℒ,V,D,E}, tβ::EmpiricalTransiogram{ℒ,V,D,E}) where {ℒ,V,D,E}
+  nα = tα.counts
+  nβ = tβ.counts
+  xα = tα.abscissas
+  xβ = tβ.abscissas
+  Yα = tα.ordinates
+  Yβ = tβ.ordinates
+
+  # copy distance and estimator
+  d = tα.distance
+  e = tα.estimator
+
+  # merge function for estimator
+  mergefun(yα, nα, yβ, nβ) = combine(e, yα, nα, yβ, nβ)
+
+  # merge coordinates and bin counts
+  n = nα + nβ
+  x = @. (xα * nα + xβ * nβ) / n
+  Y = map(zip(Yα, Yβ)) do (yα, yβ)
+    @. mergefun(yα, nα, yβ, nβ)
+  end
+
+  # adjust empty bins
+  x[n .== 0] .= xα[n .== 0]
+  for y in Y
+    y[n .== 0] .= 0
+  end
+
+  EmpiricalTransiogram(n, x, Y, d, e)
+end
