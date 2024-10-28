681712569f3691edd1de44d18583bd934acfa54d
juliohm <3345261+juliohm@users.noreply.github.com>
2024-10-28 20:41:08 +0000

    :robot: Format .jl files

--- src/fitting/transiograms.jl
+++ src/fitting/transiograms.jl
@@ -2,11 +2,7 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-function _fit(
-  T::Type{<:PiecewiseLinearTransiogram},
-  t::EmpiricalTransiogram,
-  ::FitAlgo
-)
+function _fit(T::Type{<:PiecewiseLinearTransiogram}, t::EmpiricalTransiogram, ::FitAlgo)
   # direct construction
   T(t.abscissas, t.ordinates)
 end
