6514b3f0c9f5fcea4629ed1021d1f2fe8928d9a8
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-10-28 16:42:06 -0300

    Refactor piecewiselinear.jl

--- src/theoretical/transiogram/piecewiselinear.jl
+++ src/theoretical/transiogram/piecewiselinear.jl
@@ -36,7 +36,7 @@ function PiecewiseLinearTransiogram(abscissas::AbstractVector, ordinates::Abstra
     SMatrix{m,m}(ordinates[i, j][k] for i in 1:m, j in 1:m)
   end
 
-  # proportion matrix
+  # ordinate matrix at infinity
   p = normalize(diag(last(O)), 1)
   ∞ = SMatrix{m,m}(p[j] for i in 1:m, j in 1:m)
 
