d204e721f87db339387179c73ebc8b8534c9b874
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-02-10 16:08:59 -0300

    Refactor plot recipes

--- ext/GeoStatsFunctionsMakieExt/funplot.jl
+++ ext/GeoStatsFunctionsMakieExt/funplot.jl
@@ -13,41 +13,8 @@ function _funplot(
   maxlag=nothing,
   levels=nothing
 )
-  # evaluate at rotated basis
-  hs, N, F = _evalbasis(f, maxlag)
-
-  # aesthetic options
-  label = Dict(1 => "1st axis", 2 => "2nd axis", 3 => "3rd axis")
-  style = Dict(1 => :solid, 2 => :dash, 3 => :dot)
-  level = isnothing(levels) ? (1:N) : levels
-
-  fig = Makie.Figure()
-  for i in 1:N, j in 1:N
-    lᵢ, lⱼ = level[i], level[j]
-    title = N > 1 ? "$lᵢ → $lⱼ" : ""
-    ax = Makie.Axis(fig[i, j], title = title)
-    for (k, Fₖ) in enumerate(F)
-      Makie.lines!(ax, hs, Fₖ[i, j], color=color, linewidth=size, linestyle=style[k], label=label[k])
-    end
-    position = (i == j) && isbanded(f) ? :rt : :rb
-    length(F) > 1 && Makie.axislegend(position=position)
-  end
-  fig
-end
-
-# ----------------
-# SPECIALIZATIONS
-# ----------------
-
-include("funplot/variogram.jl")
-include("funplot/transiogram.jl")
-
-# -----------------
-# HELPER FUNCTIONS
-# -----------------
-
-function _evalbasis(f, maxlag)
   # auxiliary parameters
+  N = nvariates(f)
   b = metricball(f)
   R = rotation(b)
   r = radii(b)
@@ -68,14 +35,35 @@ function _evalbasis(f, maxlag)
   # lag range starting at 1e-6 to avoid nugget artifact
   hs = range(1e-6 * oneunit(H), stop=H, length=100)
 
-  # number of variates
-  N = nvariates(f)
-
-  # reshape results
+  # evaluate function
   F = map(v) do vⱼ
     fs = [f(p, p + ustrip(h) * vⱼ) for h in hs]
     [getindex.(fs, i, j) for i in 1:N, j in 1:N]
   end
 
-  hs, N, F
+  # aesthetic options
+  label = Dict(1 => "1st axis", 2 => "2nd axis", 3 => "3rd axis")
+  style = Dict(1 => :solid, 2 => :dash, 3 => :dot)
+  level = isnothing(levels) ? (1:N) : levels
+
+  # build figure
+  fig = Makie.Figure()
+  for i in 1:N, j in 1:N
+    lᵢ, lⱼ = level[i], level[j]
+    title = N > 1 ? "$lᵢ → $lⱼ" : ""
+    ax = Makie.Axis(fig[i, j], title = title)
+    for (k, Fₖ) in enumerate(F)
+      Makie.lines!(ax, hs, Fₖ[i, j], color=color, linewidth=size, linestyle=style[k], label=label[k])
+    end
+    position = (i == j) && isbanded(f) ? :rt : :rb
+    length(F) > 1 && Makie.axislegend(position=position)
+  end
+  fig
 end
+
+# ----------------
+# SPECIALIZATIONS
+# ----------------
+
+include("funplot/variogram.jl")
+include("funplot/transiogram.jl")
