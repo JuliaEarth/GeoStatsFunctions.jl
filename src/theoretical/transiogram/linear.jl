e102875c0bb9f7a7fa0973e22b905a518aac6fdf
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-02-21 10:25:36 -0300

    Remove epsilon from transiogram models

--- src/theoretical/transiogram/linear.jl
+++ src/theoretical/transiogram/linear.jl
@@ -36,9 +36,8 @@ function (t::LinearTransiogram)(h)
   h′, r′ = unitless(h, r)
   v = h′ / r′
   T = typeof(p[1] * v)
-  ϵ = T(1e-6) # add small eps for numerical stability
   SMatrix{L,L}(
-    i == j ? (h′ < r′) * T(1 - (1 - p[j]) * v) + (h′ ≥ r′) * T(p[j]) - ϵ * (L - 1) :
-    (h′ < r′) * T(p[j] * v) + (h′ ≥ r′) * T(p[j]) + ϵ for i in 1:L, j in 1:L
+    i == j ? (h′ < r′) * T(1 - (1 - p[j]) * v) + (h′ ≥ r′) * T(p[j]) : (h′ < r′) * T(p[j] * v) + (h′ ≥ r′) * T(p[j]) for
+    i in 1:L, j in 1:L
   )
 end
