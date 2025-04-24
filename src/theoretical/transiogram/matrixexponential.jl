b5f2389a323080c0f617a01447807aab665dfbbe
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-04-24 15:12:05 -0300

    Add more constructors to MatrixExponentialTransiogram

--- src/theoretical/transiogram/matrixexponential.jl
+++ src/theoretical/transiogram/matrixexponential.jl
@@ -3,30 +3,33 @@
 # ------------------------------------------------------------------
 
 """
-    MatrixExponentialTransiogram(rate)
-
-A matrix-exponential transiogram with transition `rate` matrix.
-
-    MatrixExponentialTransiogram(; range, lengths, proportions)
-
-Alternatively, build Euclidean ball with given `range` and
-base transition rate matrix from mean `lengths` and relative
-`proportions`.
-
-    MatrixExponentialTransiogram(; ranges, rotation, lengths, proportions)
-
-Alternatively, use multiple `ranges` and `rotation` matrix
-to construct an anisotropic model.
-
     MatrixExponentialTransiogram(ball, rate)
 
-Alternatively, use a custom metric `ball`.
+A matrix-exponential transiogram with given metric `ball` and
+transition `rate` matrix.
+
+    MatrixExponentialTransiogram(ball; lengths, proportions)
+
+Alternatively, build transition `rate` matrix from nominal `lengths`
+and relative `proportions`.
+
+    MatrixExponentialTransiogram(rate; range)
+    MatrixExponentialTransiogram(rate; ranges, rotation)
+
+Alternatively, build metric `ball` from single `range` or from
+multiple `ranges` and `rotation` matrix.
+
+    MatrixExponentialTransiogram(; range, lengths, proportions)
+    MatrixExponentialTransiogram(; ranges, rotation, lengths, proportions)
+
+Alternatively, build metric `ball` and transition `rate` matrix
+from combination of previous parameters.
 
 ## Examples
 
 ```julia
-MatrixExponentialTransiogram(lengths=(3.0, 2.0, 1.0))
-MatrixExponentialTransiogram(proportions=(0.5, 0.5))
+MatrixExponentialTransiogram(lengths=(3.0, 2.0, 1.0), proportions=(1/3, 1/3, 1/3))
+MatrixExponentialTransiogram(ranges=(2.0, 1.0))
 ```
 
 ## References
@@ -61,13 +64,11 @@ function MatrixExponentialTransiogram(ball::MetricBall, rate::AbstractMatrix)
   MatrixExponentialTransiogram{typeof(ball),typeof(srate)}(ball, srate)
 end
 
-function MatrixExponentialTransiogram(rate::AbstractMatrix)
-  ball = MetricBall(1 / unit(eltype(rate)))
-  MatrixExponentialTransiogram(ball, rate)
-end
-
 MatrixExponentialTransiogram(ball::MetricBall; lengths=(1.0, 1.0), proportions=(0.5, 0.5)) =
   MatrixExponentialTransiogram(ball, baseratematrix(lengths, proportions))
+
+MatrixExponentialTransiogram(rate::AbstractMatrix; range=1.0, ranges=nothing, rotation=I) =
+  MatrixExponentialTransiogram(rangeball(range, ranges, rotation), rate)
 
 MatrixExponentialTransiogram(; range=1.0, ranges=nothing, rotation=I, lengths=(1.0, 1.0), proportions=(0.5, 0.5)) =
   MatrixExponentialTransiogram(rangeball(range, ranges, rotation), baseratematrix(lengths, proportions))
