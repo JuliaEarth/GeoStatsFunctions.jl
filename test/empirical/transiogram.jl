dc3a33f65fcf931cc026bcbdf2ba3fb81a455332
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-06-11 16:49:27 -0300

    Fix EmpiricalTransiogram in the presence of extra columns (#83)

--- test/empirical/transiogram.jl
+++ test/empirical/transiogram.jl
@@ -14,4 +14,13 @@
     ys = t.ordinates[i, i]
     @test ys[1] > ys[10] > ys[20]
   end
+
+  # additional columns don't affect results
+  csv = CSV.File(joinpath(datadir, "facies5.csv"))
+  tab = (X=csv.X, Y=csv.Y, Z=csv.Z, FACIES=csv.FACIES, NAME="FACIES" .* string.(csv.FACIES))
+  gtb1 = georef(csv, ("X", "Y", "Z"))
+  gtb2 = georef(tab, ("X", "Y", "Z"))
+  t1 = EmpiricalTransiogram(gtb1, "FACIES", maxlag=20, nlags=20)
+  t2 = EmpiricalTransiogram(gtb2, "FACIES", maxlag=20, nlags=20)
+  @test t1.ordinates == t2.ordinates
 end
